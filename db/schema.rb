61fded2b0fa1d11628d1c927e462869ebc87f81a
alexa08 <alexaa0088@gmail.com>
2015-10-09 18:46:14 +0000

    Added Subject & Section tables & models. Created subjects controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151008235720) do
+ActiveRecord::Schema.define(version: 20151009174910) do
+
+  create_table "admin_users", force: :cascade do |t|
+    t.string   "first_name",      limit: 25
+    t.string   "last_name",       limit: 50
+    t.string   "email",                      default: "", null: false
+    t.string   "username",        limit: 25
+    t.string   "hashed_password", limit: 40
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
+  end
+
+  add_index "admin_users", ["username"], name: "index_admin_users_on_username"
+
+  create_table "admin_users_pages", id: false, force: :cascade do |t|
+    t.integer "admin_user_id"
+    t.integer "page_id"
+  end
+
+  add_index "admin_users_pages", ["admin_user_id", "page_id"], name: "index_admin_users_pages_on_admin_user_id_and_page_id"
 
   create_table "pages", force: :cascade do |t|
     t.integer  "subject_id"
@@ -24,6 +43,16 @@ ActiveRecord::Schema.define(version: 20151008235720) do
 
   add_index "pages", ["permalink"], name: "index_pages_on_permalink"
   add_index "pages", ["subject_id"], name: "index_pages_on_subject_id"
+
+  create_table "section_edits", force: :cascade do |t|
+    t.integer  "admin_user_id"
+    t.integer  "section_id"
+    t.string   "summary"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "section_edits", ["admin_user_id", "section_id"], name: "index_section_edits_on_admin_user_id_and_section_id"
 
   create_table "sections", force: :cascade do |t|
     t.integer  "page_id"
