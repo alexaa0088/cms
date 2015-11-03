2f41a862878ac7cbbbb0a3039760d918c9259791
alexa08 <alexaa0088@gmail.com>
2015-11-03 15:40:00 +0000

    layout update

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,5 +1,7 @@
 class AdminUser < ActiveRecord::Base
     
+    has_secure_password
+    
     has_and_belongs_to_many :pages
     has_many :section_edits
     has_many :sections, :through => :section_edits
@@ -33,6 +35,14 @@ class AdminUser < ActiveRecord::Base
     
     validate :username_is_allowed
     #validate :no_new_users_on_saturday, :on => :create
+    
+    scope :sorted, lambda { order("last_name ASC, first_name ASC") }
+    
+    def name
+      "#{first_name} #{last_name}"
+    end
+   
+    private
     
     def username_is_allowed
         if FORBIDDEN_USERNAMES.include?(username)
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,6 +2,8 @@ class Subject < ActiveRecord::Base
   
     has_many :pages
     
+    acts_as_list
+    
     # Don't need to validate (in most cases):
     #   ids, foreign keys, timestamps, booleans, counters
     validates_presence_of :name
@@ -10,12 +12,12 @@ class Subject < ActiveRecord::Base
     # different error messages: "can't be blank" or "is too short"
     # validates_length_of allows strings with only spaces!
     
-    scope :visible, lambda { where(:visible => ture) }
+    scope :visible, lambda { where(:visible => true) }
     scope :invisible, lambda { where(:visible => false) }
     scope :sorted, lambda { order("subjects.position ASC") }
-    scope :newest_first, lambda { order("subjects.created_at DESC") }
+    scope :newest_first, lambda { order("subjects.created_at DESC")}
     scope :search, lambda {|query|
-      where(["name LIKE ?", "%#{query}%"])
+    where(["name LIKE ?", "%#{query}%"])
     }
     
 end
